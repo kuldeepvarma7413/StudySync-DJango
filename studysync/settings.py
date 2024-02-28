c0dd9ad01bc45f657cfe9898210e880a19ecfad0
Kul Deep varma <kuldeepvarma7413@gmail.com>
2024-02-28 21:25:34 +0530

    updated python version for runtime in vercel

--- studysync/settings.py
+++ studysync/settings.py
@@ -187,7 +187,7 @@ EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = "mdzaidtalha6393@gmail.com"
 EMAIL_HOST_PASSWORD = "mgbt ydvh eorn aycg"
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # cloudinary storage configration
